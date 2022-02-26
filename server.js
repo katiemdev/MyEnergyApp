e4f5c52892b21dd8291f2524170edb4ecb55c743
katiemdev <77126308+katiemdev@users.noreply.github.com>
2022-02-26 12:16:18 +0000

    connected to database

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./db-config");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 app.get("/", (req, res) => {
 	res.send("Hello");

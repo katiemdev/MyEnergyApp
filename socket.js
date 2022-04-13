457a12579ff6c9349f50b828dffcad02f441825a
katiemdev <77126308+katiemdev@users.noreply.github.com>
2022-04-13 14:46:49 +0100

    Added socket io

--- socket.js
+++
@@ -1,9 +0,0 @@
-const { updateEnergyUsage } = require("./controllers/monitorController");
-
-io.on("connection", (socket) => {
-	console.log(`New connection: ${socket.id}`);
-
-	socket.emit("updateEnergyUsage", "energy usage updated");
-
-	socket.on("disconnect", () => console.log("disconnected"));
-});
